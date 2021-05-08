f6580f736c62eb542d5a559a8596c9f4e2a70c33
mushypicker <aggris2@gmail.com>
2021-05-08 03:15:21 +0100

    2. NFT Web3 app

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,109 @@ import React, { Component } from 'react';
 import Web3 from 'web3';
 import Identicon from 'identicon.js';
 import './App.css';
-import Decentragram from '../abis/Decentragram.json'
+import Nft from '../abis/Nft.json'
 import Navbar from './Navbar'
 import Main from './Main'
 
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port:5001, protocol: 'https' })
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.requestAccount()
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+    async requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  async loadWeb3() {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+    }
+    else if(window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else{
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+
+    const networkId = await web3.eth.net.getId()
+    const networkData = Nft.networks[networkId]
+    if(networkData) {
+      const nft = web3.eth.Contract(Nft.abi, networkData.address)
+      this.setState({ nft })
+      const imagesCount = await nft.methods.imageCount().call()
+      this.setState({ imagesCount })
+
+      for (var i = 1; i <= imagesCount; i++) {
+        const image = await nft.methods.images(i).call()
+        this.setState({
+          images: [...this.state.images, image]
+        })
+      }
+
+      this.setState({
+        images: this.state.images.sort((a,b) => b.tipAmount - a.tipAmount)
+      })
+
+      this.setState({ loading: false})
+    } else {
+      window.alert('Nft contract not deployed to detected network.')
+    } 
+  }
+
+  captureFile = event => {
+    event.preventDefault()
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result) })
+      console.log('buffer', this.state.buffer)
+    }
+  }
+
+  uploadImage = description => {
+    console.log("Submitting file to ipfs")
+    ipfs.add(this.state.buffer, (error, result) => {
+      console.log('Ipfs result', result)
+      if(error) {
+        console.error(error)
+        return
+      }
+
+      this.setState({ loading: true })
+      this.state.nft.methods.uploadImage(result[0].hash, description).send({ from: this.state.account }).on('transactionHash', (hash) => {
+        this.setState({ loading: false })
+      })
+    })
+  }
+
+  tipImageOwner = (id, tipAmount) => {
+    this.setState({ loading: true })
+    this.state.nft.methods.tipImageOwner(id).send({ from: this.state.account, value: tipAmount }).on('transactionHash', (hash) => {
+      this.setState({ loading: false })
+    })
+  }
 
   constructor(props) {
     super(props)
     this.state = {
       account: '',
+      nft: null,
+      images: [],
+      loading: true
     }
   }
 
@@ -23,7 +115,10 @@ class App extends Component {
         { this.state.loading
           ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
           : <Main
-            // Code...
+              images={this.state.images}
+              captureFile={this.captureFile}
+              uploadImage={this.uploadImage}
+              tipImageOwner={this.tipImageOwner}
             />
           }
         }
